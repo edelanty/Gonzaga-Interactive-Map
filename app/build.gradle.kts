400d52e8c1d9bf63dda4667d48d6bf4792c8b162
Evan <efdelanty@gmail.com>
2024-11-10 13:07:50 -0800

    Added the favorite button to the activity comment rating activity.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.play.services.maps)
+    implementation(libs.firebase.firestore.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/aeefinalgroupproject/CommentRatingActivity.kt
+++ app/src/main/java/com/example/aeefinalgroupproject/CommentRatingActivity.kt
@@ -1,15 +1,20 @@
 package com.example.aeefinalgroupproject
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class CommentRatingActivity : AppCompatActivity() {
+    private var isFavorited = false
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_comment_rating)
@@ -25,6 +30,7 @@ class CommentRatingActivity : AppCompatActivity() {
         val ratingTextView = findViewById<TextView>(R.id.ratingTextView)
         val locationImageView = findViewById<ImageView>(R.id.locationImageView)
 
+        //TODO instead of using these locally found ones through the intent just query the database
         locationNameTextView.text = locationName
         descriptionTextView.text = description
         ratingTextView.text = "Rating: $rating"
@@ -32,6 +38,17 @@ class CommentRatingActivity : AppCompatActivity() {
         val backButton: ImageButton = findViewById(R.id.back_button)
         backButton.setOnClickListener {
             finish()
+        }
+
+        //Switches out the drawable displayed depending on whether the pin is a favorite or not
+        val favoriteButton: ImageButton = findViewById(R.id.favorite_button)
+        favoriteButton.setOnClickListener {
+            isFavorited = !isFavorited
+            val icon = if (isFavorited) R.drawable.heart_filled else R.drawable.favorites_heart
+            favoriteButton.setImageResource(icon)
+            Toast.makeText(this, if (isFavorited) "Added to favorites" else "Removed from favorites", Toast.LENGTH_SHORT).show()
+
+            //TODO add the boolean isFavorited to the database and use that instead of the one declared at the top
         }
 
         //TODO setup images properly
